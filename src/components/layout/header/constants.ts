6e4ed5a5f1e1700eedbd04b1741e03200210c5b2
delciocapolo <delciocapolo096@gmail.com>
2026-01-08 09:57:21 +0100

    feat: add Service and ServiceContainer components; enhance footer styling and update header menu items

--- src/components/layout/header/constants.ts
+++ src/components/layout/header/constants.ts
@@ -4,7 +4,26 @@ export const HEADER_APP_MENU_ITEMS = [
         label: "Serviços", 
         url: "/services",
         submenu: [
-            { label: "Delcio", url: "/hungry" },
+            { 
+                label: "ERP",
+                url: "/services/erp",
+                description: "Beautifully designed components built with Tailwind CSS."
+            },
+            { 
+                label: "Design", 
+                url: "/services/design",
+                description: "Acreditamos que um bom design não é apenas uma questão de estética; trata-se de criar experiências intuitivas e fluidas para o usuário.",
+            },
+            { 
+                label: "Engenharia de Software", 
+                url: "/services/engineering",
+                description: "Seguimos metodologias padrão do setor e empregamos ferramentas eficazes de comunicação e colaboração para mantê-lo informado durante todo o processo de desenvolvimento.",
+            },
+            { 
+                label: "Gestão de Projectos", 
+                url: "/services/project-management",
+                description: "How to install dependencies and structure your app.",
+            },
         ]
     },
     { label: "Portfólio", url: "/portfolio" },
