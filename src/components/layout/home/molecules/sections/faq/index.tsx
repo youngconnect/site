c3459e7743f544862416ec43a0af37343906fec6
delciocapolo <delciocapolo096@gmail.com>
2026-07-02 11:34:21 +0100

    feat: enhance FAQ component layout and loading state handling

--- src/components/layout/home/molecules/sections/faq/index.tsx
+++ src/components/layout/home/molecules/sections/faq/index.tsx
@@ -35,35 +35,36 @@ export default function FAQComponent() {
             <article className="container mx-auto">
                 <Accordion type="single" collapsible className="columns-1 md:columns-2 space-y-4 p-10">
                     {
-                        isLoadingAllFaq ? <span className="">Carregando Componente</span> : 
-                        faqs?.map((faq, index) => (
-                            <AccordionItem key={index} value={`faq-${index}`} className="border-none px-10 py-5 ring ring-gray-700/65">
-                                <AccordionTrigger 
-                                    className="group hover:no-underline items-center"
-                                    expandIcon={<Icon icon={"ic:baseline-plus"} className="group-data-[state=open]:text-primary-900 pointer-events-none size-8 shrink-0 translate-y-0.5 transition-transform duration-200" /> }
-                                >
-                                    <div className="flex-center gap-5">
-                                        <div className="flex-center">
-                                            <span className="size-16 rounded-md ring ring-gray-700/45 flex-center text-headline-24 bg-gray-800/65 overflow-hidden relative group-data-[state=open]:text-primary-900">
-                                                <div className="absolute inset-0 bg-[radial-gradient(circle_at_top_right,rgba(196,196,196,0.10)_0%,transparent_55%)]" />
-                                                { String(index + 1).padStart(2, "0") }
-                                            </span>
+                        isLoadingAllFaq ? 
+                            <span className="">Carregando Componente</span> : 
+                            faqs?.map((faq, index) => (
+                                <AccordionItem key={index} value={`faq-${index}`} className="border-none px-10 py-5 ring ring-gray-700/65">
+                                    <AccordionTrigger 
+                                        className="group hover:no-underline items-center"
+                                        expandIcon={<Icon icon={"ic:baseline-plus"} className="group-data-[state=open]:text-primary-900 pointer-events-none size-8 shrink-0 translate-y-0.5 transition-transform duration-200" /> }
+                                    >
+                                        <div className="flex-center gap-5">
+                                            <div className="flex-center">
+                                                <span className="size-16 rounded-md ring ring-gray-700/45 flex-center text-headline-24 bg-gray-800/65 overflow-hidden relative group-data-[state=open]:text-primary-900">
+                                                    <div className="absolute inset-0 bg-[radial-gradient(circle_at_top_right,rgba(196,196,196,0.10)_0%,transparent_55%)]" />
+                                                    { String(index + 1).padStart(2, "0") }
+                                                </span>
+                                            </div>
+                                            <div className="py-2">
+                                                <h1 className="text-body-16 font-bold group-data-[state=open]:text-primary-900">{faq.title}</h1>
+                                            </div>
                                         </div>
-                                        <div className="py-2">
-                                            <h1 className="text-body-16 font-bold group-data-[state=open]:text-primary-900">{faq.title}</h1>
+                                    </AccordionTrigger>
+                                    <AccordionContent>
+                                        <div className="w-full flex gap-5">
+                                            <div className="flex-none w-18" />
+                                            <div className="flex-1">
+                                                <p className="">{faq.content}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </AccordionTrigger>
-                                <AccordionContent>
-                                    <div className="w-full flex gap-5">
-                                        <div className="flex-none w-18" />
-                                        <div className="flex-1">
-                                            <p className="">{faq.content}</p>
-                                        </div>
-                                    </div>
-                                </AccordionContent>
-                            </AccordionItem>
-                        ))
+                                    </AccordionContent>
+                                </AccordionItem>
+                            ))
                     }
                 </Accordion>
             </article>
